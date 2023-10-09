750015f35eb57e01493260be0cb4836e235bd38e
Maurits Misana <maurits@techmaus.nl>
2023-10-09 16:42:16 +0200

    Move unrelated UI components to index.tsx file

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,13 @@ export default function Home() {
 
     return (
         <main className="bg-gray-50 py-12 min-w-full min-h-full">
-            <MetadataDistributions distributions={distributions} />
+            <div className="max-w-sm mx-auto">
+                <MetadataDistributions distributions={distributions} />
+            </div>
+
+            <pre className="mt-12 text-xs">
+                {JSON.stringify(distributions, null, 2)}
+            </pre>
         </main>
     )
 }
