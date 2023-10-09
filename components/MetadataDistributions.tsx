b36ed7c116a6f3d6c9f55f476226db473b09077d
Maurits Misana <maurits@techmaus.nl>
2023-10-09 16:40:30 +0200

    Fix missing requirement of showing 10th segment like a regular one.

--- components/MetadataDistributions.tsx
+++ components/MetadataDistributions.tsx
@@ -1,6 +1,7 @@
 import Tooltip, { useSingleton } from '@/components/Tooltip'
 import Link from 'next/link'
 import { DataFromApi } from 'types/data'
+import clsx from 'clsx'
 
 const colors = [
     {
@@ -41,11 +42,59 @@ const colors = [
     },
 ]
 
+function BarSegment({
+    type,
+    className,
+    label,
+    value,
+    total,
+    target,
+}: {
+    type: 'regular' | 'other'
+    className?: string
+    label: string
+    value: number
+    total: number
+    target: ReturnType<typeof useSingleton>[1]
+}) {
+    const fractionFormatter = new Intl.NumberFormat('en-US', {
+        style: 'percent',
+        maximumFractionDigits: 1,
+    })
+
+    return (
+        <Tooltip
+            content={`${label} (${fractionFormatter.format(value / total)})`}
+            singleton={target}
+        >
+            {type === 'regular' ? (
+                <Link
+                    href="#"
+                    style={{
+                        ['--segment-value' as string]: value,
+                    }}
+                    className={clsx(
+                        className,
+                        'h-full w-[calc(100%*(var(--segment-value)/var(--total-distribution-width)))]'
+                    )}
+                >
+                    <span className="sr-only">{`${label}: ${value}`}</span>
+                </Link>
+            ) : (
+                // Use flex-1 to fill the remaining space
+                <div className={clsx(className, 'h-full flex-1')} />
+            )}
+        </Tooltip>
+    )
+}
+
 function Bar({
     index,
+    total,
     distributions,
 }: {
     index: number
+    total: number
     distributions: DataFromApi['app']['metadataDistributions'][number]['distributions']
 }) {
     const colorGroup = colors[index % colors.length]
@@ -54,39 +103,60 @@ function Bar({
     const visibleDistributions = distributions.slice(0, 9)
     // Group the rest of the distributions into one
     const restDistribution = distributions.slice(9)
+    // Calculate the total value of the rest distribution
+    const totalRestValue = restDistribution.reduce(
+        (acc, { value }) => acc + value,
+        0
+    )
 
+    // Use a singleton to share the same tooltip instance
     const [source, target] = useSingleton()
 
     return (
         <div className="flex rounded-full overflow-hidden h-2 w-full gap-x-px">
+            {/* We first iterate over the regular bar parts */}
             {visibleDistributions.map(({ key, value }, index) => {
-                // @ts-expect-error We know that colorGroup is an object with keys 0-9
+                // @ts-expect-error - We know that the index is always a number between 0 and 8
                 const bgColor = colorGroup[index]
 
                 return (
-                    <Tooltip
+                    <BarSegment
                         key={key}
-                        content={`${key} (${value}%)`}
-                        singleton={target}
-                    >
-                        <Link
-                            href="#"
-                            style={{
-                                ['--segment-value' as string]: value,
-                            }}
-                            className={`${bgColor} h-full w-[calc(100%*(var(--segment-value)/var(--total-distribution-width)))]`}
-                        >
-                            <span className="sr-only">{`${key}: ${value}`}</span>
-                        </Link>
-                    </Tooltip>
+                        className={bgColor}
+                        label={key}
+                        target={target}
+                        total={total}
+                        type="regular"
+                        value={value}
+                    />
                 )
             })}
-            {restDistribution.length > 0 ? (
-                // Use flex-1 to fill the remaining space
-                <div className={`${colorGroup[9]} h-full flex-1`} />
+
+            {/* If there is only a 10th segment, we can add it just like a regular segment. */}
+            {restDistribution.length === 1 ? (
+                <BarSegment
+                    key={restDistribution[0].key}
+                    className={colorGroup[9]}
+                    label={restDistribution[0].key}
+                    target={target}
+                    total={total}
+                    type="regular"
+                    value={restDistribution[0].value}
+                />
             ) : null}
 
-            {/* Use a singleton to share the same tooltip instance */}
+            {/* Otherwise we cluster the remaining parts into one 'other' segment */}
+            {restDistribution.length > 1 ? (
+                <BarSegment
+                    className={colorGroup[9]}
+                    label={`${restDistribution.length} others...`}
+                    target={target}
+                    total={total}
+                    type="other"
+                    value={totalRestValue}
+                />
+            ) : null}
+
             <Tooltip singleton={source} />
         </div>
     )
@@ -107,7 +177,11 @@ function MetadataDistribution({
             className={`px-6 pt-3.5 pb-4 flex flex-col gap-y-3`}
         >
             <h2 className="text-sm capitalize leading-none">{`${distribution.name} (${distribution.unique})`}</h2>
-            <Bar index={index} distributions={distribution.distributions} />
+            <Bar
+                index={index}
+                distributions={distribution.distributions}
+                total={distribution.total}
+            />
         </div>
     )
 }
@@ -145,7 +219,7 @@ export default function MetadataDistributions({
                 </div>
             </div>
 
-            <pre className="mt-12">
+            <pre className="mt-12 text-xs">
                 {JSON.stringify(distributions, null, 2)}
             </pre>
         </>
